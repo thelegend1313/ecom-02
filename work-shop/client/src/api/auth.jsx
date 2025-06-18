76996a97b3aac3b264ef1f63288d67d149b1f732
thelegend1313 <interiorlegend1313@gmail.com>
2025-06-18 11:34:58 +0700

    test

--- work-shop/client/src/api/auth.jsx
+++ work-shop/client/src/api/auth.jsx
@@ -7,7 +7,7 @@ export const currentUser = async(token) => {
         }
     })
 }
-//test commit
+//test commit 2
     export const currentAdmin = async(token) => {
         return await axios.post('http://localhost:5001/api/current-admin',{},{
         headers:{
