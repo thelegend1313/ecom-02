4608192b8d226befdfb46bd5ede86f7b6f129260
ecom2 <ingteriorlegend1313@gmail.com>
2025-07-21 23:24:13 +0700

    st

--- work-shop/client/src/pages/card/CheckoutForm.jsx
+++ work-shop/client/src/pages/card/CheckoutForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import '../../stripe.css'
 import {
   PaymentElement,
   useStripe,
@@ -23,23 +24,24 @@ export default function CheckoutForm() {
 
     setIsLoading(true);
 
-    const { error } = await stripe.confirmPayment({
+    const payload = await stripe.confirmPayment({
       elements,
-      confirmParams: {
+      /* confirmParams: {
         // Make sure to change this to your payment completion page
         return_url: "http://localhost:3000/complete",
-      },
+      }, */
+      redirect:"if_required"
     });
-
+console.log(payload)
     // This point will only be reached if there is an immediate error when
     // confirming the payment. Otherwise, your customer will be redirected to
     // your `return_url`. For some payment methods like iDEAL, your customer will
     // be redirected to an intermediate site first to authorize the payment, then
     // redirected to the `return_url`.
-    if (error.type === "card_error" || error.type === "validation_error") {
+    if (payload.error) {
       setMessage(error.message);
     } else {
-      setMessage("An unexpected error occurred.");
+      console.log("Jukku",payload);
     }
 
     setIsLoading(false);
@@ -50,10 +52,12 @@ export default function CheckoutForm() {
   }
 
   return (
-    <form id="payment-form" onSubmit={handleSubmit}>
+    <form className="space-y-6"
+    id="payment-form" onSubmit={handleSubmit}>
 
       <PaymentElement id="payment-element" options={paymentElementOptions} />
-      <button disabled={isLoading || !stripe || !elements} id="submit">
+      <button className="stripe-button"
+      disabled={isLoading || !stripe || !elements} id="submit">
         <span id="button-text">
           {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
         </span>
